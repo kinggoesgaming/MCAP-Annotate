fb763f5fa06a0726723fab6be772d94a3c7701d5
kinggoesgaming <hunar.roop@gmail.com>
2016-09-23 20:26:45 -0700

    add Catalog annotation

---
+++ src/main/java/io/github/minigamecore/mcap/annotate/catalog/Catalog.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of MCAP-Annotate, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016 - 2016 MinigameCore <http://minigamecore.github.io>
+ * Copyright (c) Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package io.github.minigamecore.mcap.annotate.catalog;
+
+import org.spongepowered.api.CatalogType;
+
+/**
+ * Annotation marking a {@link CatalogType} implementation.
+ */
+public @interface Catalog {
+
+    /**
+     * The {@link CatalogType} class.
+     *
+     * <p>
+     *     Cannot be a {@link CatalogType} already provided by Sponge.
+     * </p>
+     *
+     * @return The CatalogType class.
+     */
+    Class<? extends CatalogType> catalogTypeClass();
+
+    /**
+     * The class containing the {@link #field()} for which the {@link CatalogType} value has to be changed.
+     *
+     * @return The container class.
+     */
+    Class containerClass();
+
+    /**
+     * The {@link CatalogType} field name.
+     *
+     * <p>
+     *     The field should have {@code public static final} modifiers.
+     * </p>
+     *
+     * @return The field name.
+     */
+    String field();
+
+}
